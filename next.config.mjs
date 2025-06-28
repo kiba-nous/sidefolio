ae20d88a71560fdaff50a481f2f9b8864e574dc9
kiba-nous <syazwaninaqibah@gmail.com>
2025-06-29 01:16:34 +0800

    Fix network fetch error by using local placeholder images

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ import rehypePrism from "@mapbox/rehype-prism";
 
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "res.cloudinary.com"],
+    domains: ["images.pexels.com", "res.cloudinary.com"],
   },
   experimental: {
     mdxRs: true,
